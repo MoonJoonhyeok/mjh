742a3d610e4f03923b893c593394ea27ae756f3f
MoonJoonhyeok <104426867+MoonJoonhyeok@users.noreply.github.com>
2022-07-25 07:29:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-import pandas as pd
-import plotly.express as px
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.graph_objects as go
-
-import numpy as np
-import matplotlib.pyplot as plt
 
 template='plotly_white'
 
